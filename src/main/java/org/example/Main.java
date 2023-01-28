feed8716c624e9b7d65dcc545ace29ea83399aca
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-01-28 14:48:57 -0300

    make the bayes learn and test a case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,9 +13,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        BayesClassifier<String, String> bayes = new BayesClassifier<>();
 
         String pathPositiveTexts = "src\\main\\resources\\positiveTexts.txt";
         String pathNegativeTexts = "src\\main\\resources\\negativeTexts.txt";
+
+        List<String> listPositiveTexts = getTexts(pathPositiveTexts);
+        List<String> listNegativeTexts = getTexts(pathNegativeTexts);
+
+        for (String text : listPositiveTexts) {
+            bayes.learn("positive", Arrays.asList(text.split("\\s")));
+        }
+
+        for (String text : listNegativeTexts) {
+            bayes.learn("negative", Arrays.asList(text.split("\\s")));
+        }
+
+        String[] unknownText =
+                Normalizer.normalize("sistema maravilhoso, me ajudou a alcançar resultados incriveis", Normalizer.Form.NFD)
+                        .replaceAll("[^\\p{ASCII}]", "")
+                        .replaceAll("[^a-zA-Z0-9\\s+]", "")
+                        .toLowerCase()
+                        .split("\\s");
+
+        System.out.println(bayes.classify(Arrays.asList(unknownText)).getCategory());
 
     }
 
