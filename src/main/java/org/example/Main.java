d96cd4a64c3a669f69b4c5039eb0c7380785e67f
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-01-28 12:02:24 -0300

    create method to get texts from the file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import de.daslaboratorium.machinelearning.classifier.Classifier;
+import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String pathPositiveTexts = "src\\main\\resources\\positiveTexts.txt";
+        String pathNegativeTexts = "src\\main\\resources\\negativeTexts.txt";
+
+    }
+
+    public static List<String> getTexts(String path){
+        List<String> listTexts = new ArrayList<>();
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
+            String line = bf.readLine();
+
+            while (line != null) {
+
+                line = Normalizer.normalize(line, Normalizer.Form.NFD)
+                        .replaceAll("[^\\p{ASCII}]", "")
+                        .replaceAll("[^a-zA-Z0-9\\s+]", "")
+                        .toLowerCase();
+
+                listTexts.add(line);
+                line = bf.readLine();
+            }
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        return listTexts;
     }
 }
